00f412eb0ca0902ff71b88d41fd40d7bd1d5d26c
Jaspreet Singh <jaspreet_xps@yahoo.com>
2016-10-14 15:57:22 -0700

    Add multi-range support

--- src/client/components/DataInputForm.js
+++ src/client/components/DataInputForm.js
@@ -4,14 +4,18 @@ import * as actions from '../actions'
 import TextField from 'material-ui/TextField'
 import CircularProgress from 'material-ui/CircularProgress'
 import RaisedButton from 'material-ui/RaisedButton'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 
 class DataInputForm extends Component {
   constructor(props) {
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleRangeAddition = this.handleRangeAddition.bind(this)
     this.handleTextChange = this.handleTextChange.bind(this)
-    this.state = { domain: '[  ]', range: '[  ]' }
+    this.generateRangeField = this.generateRangeField.bind(this)
+    this.state = { domain: '[  ]', range0: '[  ]', rangeAdditions: [] }
   }
 
   handleTextChange(event, stateKey) {
@@ -25,25 +29,56 @@ class DataInputForm extends Component {
     this.props.setVizData(this.state)
   }
 
+  handleRangeAddition() {
+    const rangeKey = `range${this.state.rangeAdditions.length + 1}`
+    const rangeAddition = this.generateRangeField(rangeKey)
+    const stateUpdate = {}
+    stateUpdate.rangeAdditions = this.state.rangeAdditions.concat(rangeAddition)
+    this.setState(stateUpdate)
+  }
+
+  generateRangeField(rangeKey) {
+    return (
+      <div key={rangeKey}>
+        <TextField onChange={(event)=>this.handleTextChange(event, rangeKey)}
+                   value={this.state[rangeKey]}
+                   fullWidth
+                   multiLine
+                   floatingLabelText={"Enter Range Value"}/>
+      </div>
+    )
+  }
+
   render() {
     const { isFetching } = this.props
     const VisualizeButton = <RaisedButton type="submit" label="Visualize" primary={true} style={{ margin: 12 }}/>
     const LoadingComponent = <CircularProgress />
+    console.log(this.state)
     return (
       <form onSubmit={this.handleSubmit} style={{ paddingLeft: 20 }}>
         <div>
           <TextField onChange={(event)=>this.handleTextChange(event, 'domain')}
                      value={this.state.domain}
                      fullWidth
+                     multiLine
                      floatingLabelText={"Domain"}/>
         </div>
         <div>
-          <TextField onChange={(event)=>this.handleTextChange(event, 'range')}
-                     value={this.state.range}
+          <TextField onChange={(event)=>this.handleTextChange(event, 'range0')}
+                     value={this.state.range0}
                      fullWidth
+                     multiLine
                      floatingLabelText={"Range"}/>
         </div>
-        <div style={{ textAlign: 'center' }}>
+        {
+          this.state.rangeAdditions
+        }
+        <div style={{ textAlign: 'center', paddingTop: 10 }}>
+          <FloatingActionButton mini onClick={this.handleRangeAddition}>
+            <ContentAdd />
+          </FloatingActionButton>
+        </div>
+        <div style={{ textAlign: 'center', paddingTop: 40 }}>
           {
             isFetching ?
             LoadingComponent :
--- src/client/components/DataVisualization.js
+++ src/client/components/DataVisualization.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
-import { isEmpty } from 'lodash'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
+import { isEmpty, times } from 'lodash'
 
 class DataVisualization extends Component {
   componentWillReceiveProps(nextProps) {
@@ -13,11 +13,25 @@ class DataVisualization extends Component {
     if (isEmpty(dataViz)) {
       return <div/>
     }
+    const numRanges = dataViz.rangeAdditions.length + 1
     const domain = JSON.parse(dataViz.domain)
-    const range = JSON.parse(dataViz.range)
-    const data = domain.map((domainDatum, index) => {
-      return { domain: domainDatum, range: range[index] }
+    const ranges = []
+    times(numRanges, (index) => {
+      ranges.push(JSON.parse(dataViz[`range${index}`]))
     })
+
+    const data = domain.map((domainDatum, index) => {
+      const entry = { domain: domainDatum }
+      times(numRanges, (rangeIndex) => {
+        const rangeKey = `range${rangeIndex}`
+        entry[rangeKey] = ranges[rangeIndex][index]
+      })
+      return entry
+    })
+
+    const Lines = []
+    _.times(numRanges, (index) => Lines.push(<Line key={index} type="monotone" dataKey={`range${index}`}
+                                                   stroke="green"/>))
 
     return (
       <LineChart width={600} height={300} data={data}
@@ -26,8 +40,10 @@ class DataVisualization extends Component {
         <YAxis/>
         <CartesianGrid strokeDasharray="3 3"/>
         <Tooltip/>
-        <Legend />
-        <Line type="monotone" dataKey="range" stroke="green"/>
+        {
+          Lines
+        }
+
       </LineChart>
     )
   }
